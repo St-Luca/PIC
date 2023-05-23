7ced3686d88d0027caa26c318c1637438e1973c5
St-Luca <104420338+St-Luca@users.noreply.github.com>
2023-05-23 17:50:50 +0500

    Добавьте файлы проекта.

---
+++ PIC/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    internal class AuthController
+    {
+        public int PasswordHash;
+        public int GetHash(string password)
+        {
+            PasswordHash = 3 * password.GetHashCode() + 42;
+            return PasswordHash;
+        }
+
+        public string GetUserData()
+        {
+
+        }
+    }
+}
---
+++ PIC/PermissionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    internal class PermissionController
+    {
+
+        public void GivePermissions(User user)
+        {
+
+        }
+    }
+}

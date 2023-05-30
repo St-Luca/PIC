0f579e97854e779ad1fa1c3a3208d6b3fa16dd6f
Иван Бекевич <stud0000261082@study.utmn.ru>
2023-05-31 01:23:11 +0500

    Add CapturedAnimalCard and CapturedAnimalCardController

---
+++ PIC/CapturedAnimalCard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    internal class CapturedAnimalCard
+    {
+        int Id { get; set; }
+        List<string> Parameters { get; set; }
+        string Coloring { get; set; }
+        string Ears { get; set; }
+        string Tail { get; set; }
+        string SpecialSigns { get; set; }
+        int IdTag { get; set; }
+        int ChipNumber { get; set; }
+        Locality Locality { get; set; }
+        int CaptureId { get; set; }
+
+        public CapturedAnimalRegister(int id, List<string> parameters, string coloring, string ears, string tail, string specialSigns, int idTag, int chipNumber, Locality locality, int captureId)
+        {
+            if (IsDataCorrect(id, parameters, coloring, ears, tail, specialSigns, idTag, chipNumber, locality, captureId))
+            {
+                Id = id;
+                Parameters = parameters;
+                Coloring = coloring;
+                Ears = ears;
+                Tail = tail;
+                SpecialSigns = specialSigns;
+                IdTag = idTag;
+                ChipNumber = chipNumber;
+                Locality = locality;
+                CaptureId = captureId;
+            }
+        }
+
+        private bool IsDataCorrect(int id, List<string> parameters, string coloring, string ears, string tail, string specialSigns, int idTag, int chipNumber, Locality locality, int captureId)
+        {
+            return true;
+        }
+    }
+}

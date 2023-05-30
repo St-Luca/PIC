00b50377cd88e9c34b3da376098dca6364c2323a
Иван Бекевич <stud0000261082@study.utmn.ru>
2023-05-31 01:30:27 +0500

    Added CaptureController classes and methods

---
+++ PIC/CaptureController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIC
+{
+    internal class CaptureController
+    {
+        public static bool CanMakeCapture(int userPermissionCode)
+        { //смотрим на код юзера, т.е. его права в системе.
+            return true;
+        }
+
+        public void AddCapture(CapturedAnimalCard captAnimal)
+        {
+            //connection with db and parsing fields
+        }
+
+        public void Delete()
+        {
+
+        }
+
+        public void ChangeCapture()
+        {
+
+        }
+    }
+}
